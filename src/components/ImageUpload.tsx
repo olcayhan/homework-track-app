8cf9498ccaf6e93cb6a22bed6f13c883abb45faa
olcayhan <kolcayhan1@gmail.com>
2024-09-26 20:35:48 +0300

    minor: image upload bug fixed

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -6,38 +6,43 @@ import { Progress } from "./ui/progress";
 
 function ImageUpload({ value, onChange }: { value: any[]; onChange: any }) {
   const [progress, setProgress] = useState<any>(null);
-  const onDrop = useCallback((acceptedFiles: any) => {
-    if (!acceptedFiles) return;
-    acceptedFiles.map((file: any) => {
-      const storageRef = ref(storage, `files/${file.name}`);
-      const uploadTask = uploadBytesResumable(storageRef, file);
 
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const prog = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          setProgress(prog);
-        },
-        (error) => {
-          console.error("Upload error:", error);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            onChange([
-              ...value,
-              { path: file.name, size: file.size, url: downloadURL },
-            ]);
-            setProgress(null);
-          });
-        }
-      );
-    });
-  }, []);
+  const onDrop = useCallback(
+    (acceptedFiles: any) => {
+      if (!acceptedFiles) return;
+      acceptedFiles.map((file: any) => {
+        const storageRef = ref(storage, `files/${Date.now() + file.name}`);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const prog =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            setProgress(prog);
+          },
+          (error) => {
+            console.error("Upload error:", error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              onChange([
+                ...value,
+                { path: file.name, size: file.size, url: downloadURL },
+              ]);
+              setProgress(null);
+            });
+          }
+        );
+      });
+    },
+    [onChange, value]
+  );
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   const files = value.map((file: any) => (
-    <li key={file.path}>
+    <li key={file.url}>
       {file.path} - {file.size} bytes -{" "}
       <a
         className="bg-blue-500 px-2 py-1 font-semibold text-white"
