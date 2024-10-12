d5967bfe1d6f70b2b70fb943a31f02b3820f4dc9
olcayhan <kolcayhan1@gmail.com>
2024-10-12 12:56:44 +0300

    feat: update CourseItem component with more button

--- src/components/course/CourseItem.tsx
+++ src/components/course/CourseItem.tsx
@@ -1,13 +1,12 @@
 import {
   Card,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "../ui/card";
 import { Button } from "../ui/button";
 import useModal from "@/hooks/useModal";
-import { Image, Pen, Trash } from "lucide-react";
+import { EllipsisVertical, Image, Pen, Trash } from "lucide-react";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -21,13 +20,74 @@ import {
 } from "../ui/alert-dialog";
 import { Course } from "@/types/Course";
 import useCourse from "@/hooks/useCourse";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 const CourseItem = ({ course }: { course: Course }) => {
   const { setOpen, isEditing } = useModal();
   const { deleteCourse } = useCourse();
   return (
     <Card>
-      <CardHeader>
+      <CardHeader className="relative">
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              size={"icon"}
+              variant={"ghost"}
+              className="absolute top-1 right-1"
+            >
+              <EllipsisVertical />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-56" align="start">
+            <div className="grid gap-4">
+              <h4 className="font-medium leading-none">Options</h4>
+              <div className="grid gap-2">
+                <Button
+                  variant="outline"
+                  onClick={() => {
+                    isEditing(true, course.id);
+                    setOpen(true);
+                  }}
+                >
+                  <Pen className="mr-2 w-4 h-4" />
+                  Edit
+                </Button>
+
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <Button className="text-white bg-red-700 hover:bg-red-800">
+                      <Trash className="mr-2 w-4 h-4" />
+                      Delete
+                    </Button>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>
+                        Are you absolutely sure?
+                      </AlertDialogTitle>
+                      <AlertDialogDescription>
+                        This action cannot be undone. This will permanently
+                        delete your account and remove your data from our
+                        servers.
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>Cancel</AlertDialogCancel>
+                      <AlertDialogAction
+                        onClick={() => {
+                          deleteCourse(course.id);
+                        }}
+                        className="bg-red-700 hover:bg-red-800"
+                      >
+                        Delete
+                      </AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+              </div>
+            </div>
+          </PopoverContent>
+        </Popover>
         {course.imageURL ? (
           <img
             className="h-60 w-60 object-contain"
@@ -44,47 +104,6 @@ const CourseItem = ({ course }: { course: Course }) => {
           dangerouslySetInnerHTML={{ __html: course.description }}
         ></CardDescription>
       </CardHeader>
-      <CardFooter className="justify-end gap-3">
-        <Button
-          variant="outline"
-          onClick={() => {
-            isEditing(true, course.id);
-            setOpen(true);
-          }}
-        >
-          <Pen className="mr-2 w-4 h-4" />
-          Edit
-        </Button>
-
-        <AlertDialog>
-          <AlertDialogTrigger asChild>
-            <Button className="text-white bg-red-700 hover:bg-red-800">
-              <Trash className="mr-2 w-4 h-4" />
-              Delete
-            </Button>
-          </AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction
-                onClick={() => {
-                  deleteCourse(course.id);
-                }}
-                className="bg-red-700 hover:bg-red-800"
-              >
-                Delete
-              </AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-      </CardFooter>
     </Card>
   );
 };
