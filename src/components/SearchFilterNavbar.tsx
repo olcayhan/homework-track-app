bf568022872f0edd1d1328ff4c33e9d52f147102
olcayhan <kolcayhan1@gmail.com>
2024-10-16 19:05:47 +0300

    feat: add SearchFilterNavbar to Assignment page and add Assignment button position changed

--- src/components/SearchFilterNavbar.tsx
+++ src/components/SearchFilterNavbar.tsx
@@ -4,7 +4,7 @@ import { Input } from "./ui/input";
 
 export default function SearchFilterNavbar() {
   return (
-    <div className="sticky bg-neutral-50 z-50 top-0 w-full flex gap-4 p-2 border-b-[1px] border-b-neutral-400">
+    <div className="sticky bg-neutral-50 z-50 top-0 w-full flex gap-4 p-3 border-b-[1px] border-b-neutral-400">
       <div className="relative w-full">
         <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-gray-500" />
         <Input placeholder="Search" type="text" className="flex-auto pl-8" />
--- src/pages/Assignment.tsx
+++ src/pages/Assignment.tsx
@@ -4,13 +4,18 @@ import { AssignmentModal } from "@/components/modals/AssignmentModal";
 import AssignmentItem from "@/components/assignment/AssignmentItem";
 import useAssignment from "@/hooks/useAssignment";
 import NotFound from "@/components/NotFound";
+import SearchFilterNavbar from "@/components/SearchFilterNavbar";
 
 export default function Assignment() {
   const assignments = useAssignment((state) => state.assignment);
 
   return (
-    <div className="w-full h-full flex flex-col justify-start items-start p-3 gap-3">
-      <AssignmentModal />
+    <div className="w-full h-full flex flex-col justify-start items-start gap-3">
+      <SearchFilterNavbar />
+      <div className="fixed right-10 bottom-10 w-64 h-12">
+        <AssignmentModal />
+      </div>
+
       {assignments.length > 0 ? (
         assignments.map((assignment) => (
           <AssignmentItem key={assignment.id} assignment={assignment} />
