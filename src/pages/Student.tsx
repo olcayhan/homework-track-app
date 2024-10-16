13143340b6e643bd18e5a96788f6cfadd9afaa53
olcayhan <kolcayhan1@gmail.com>
2024-10-16 19:07:09 +0300

    fix: unnecessary StudentModal usage removed

--- src/pages/Student.tsx
+++ src/pages/Student.tsx
@@ -1,11 +1,9 @@
-import { StudentModal } from "@/components/modals/StudentModal";
 import NotFound from "@/components/NotFound";
 import { User } from "lucide-react";
 
 export default function Student() {
   return (
     <div className="w-full h-full flex flex-col justify-start items-start p-3 gap-3">
-      <StudentModal />
       <NotFound
         Icon={User}
         title="No students found"
