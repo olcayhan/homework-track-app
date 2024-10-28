bf58bedfe39bcc6faf564a4afa74298467d07fcc
olcayhan <kolcayhan1@gmail.com>
2024-10-28 15:38:46 +0300

    feat: Add @tanstack/react-query package for API data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Toaster } from "./components/ui/toaster.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-    <Toaster />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <Toaster />
+    </QueryClientProvider>
   </StrictMode>
 );
