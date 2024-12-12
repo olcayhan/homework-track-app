5b71c061d76779a93ccbf6b24ee0496d1a8a8a0a
olcayhan <kolcayhan1@gmail.com>
2024-12-12 22:45:16 +0300

    fix: course type changed related to be.

--- src/types/Course.ts
+++ src/types/Course.ts
@@ -1,7 +1,12 @@
 export type Course = {
-    id: number;
-    name: string;
-    description: string;
-    imageURL?: string;
-  };
-  
+  $id: string;
+  code?: string;
+  description: string;
+  name: string;
+  imagePath?: string;
+  homeworks?: any;
+  id: number;
+  status: string;
+  studentCourses?: any;
+  teacherId?: number;
+};
